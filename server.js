34e1b0736acc6ccab7a8ca61e90f9defc1246f43
Razan Harashe <harashe115@gmail.com>
2024-07-02 11:39:08 +0300

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 
+app.use(cors())
+require('./servers/routers/product.routes')(app);
 app.listen(port,()=>
 console.log(`Listening on port:${port}`));
